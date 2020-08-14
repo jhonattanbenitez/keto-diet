5d033f3c8fdc4e84de6231c289b15122f6133516
Jhonattan <jhonattan.benitez@hotmail.com>
2020-08-13 23:10:36 -0500

    imreact-router-dom implementation and diet page added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,76 @@
-import React from 'react';
-import './App.css' 
-import WeekContainer from './components/WeekContainer/week-container.component';
-import days from './days.json'
+import React from "react";
+import "./App.css";
+import WeekContainer from "./components/WeekContainer/week-container.component";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import days from "./days.json";
+import DietContainer from "./pages/diet-container";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      week: []
-      
-    }; 
-    
-   
+      week: [],
+    };
   }
   componentDidMount() {
-    this.setState({ week: days })
+    this.setState({ week: days });
   }
   render() {
     return (
       <div>
         <h1>KETO DIET</h1>
-      <WeekContainer week={this.state.week} />
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+                <li>
+                  <Link to="/diet">Diet</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/diet">
+                <DietContainer />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+        <WeekContainer week={this.state.week} />
       </div>
-    )
+    );
   }
+}
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
---
+++ src/pages/diet-container.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class DietContainer extends React.Component {
+    render() {
+        return(<h1>Dieta</h1>)
+    }
+}
+
+export default DietContainer;
